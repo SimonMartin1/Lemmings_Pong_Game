d1ad64eec15be77f4ccae633366d374462b3643a
Simon Martin <simonmartinsposito08@gmail.com>
2025-04-28 01:16:16 -0300

    Added Main Classes for Game Structure

--- app/src/test/java/proyecto/AppTest.java
+++ app/src/test/java/proyecto/AppTest.java
@@ -4,11 +4,11 @@
 package proyecto;
 
 import org.junit.jupiter.api.Test;
+
+import Proyecto.App;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+
 }
