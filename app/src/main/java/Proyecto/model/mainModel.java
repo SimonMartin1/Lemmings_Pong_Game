c098f226a95687bf77c1a88039b1afbf1b85024f
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-05-17 01:04:12 -0300

    Delete class files

--- app/src/main/java/Proyecto/model/mainModel.java
+++ app/src/main/java/Proyecto/model/mainModel.java
@@ -2,20 +2,17 @@ package Proyecto.Model;
 import java.util.ArrayList;
 
 import Proyecto.Games.JGame;
+import Proyecto.Games.Lemmings_game.Lemmings;
+import Proyecto.Games.Pong_game.Pong;
 
 public class MainModel {
     final private ArrayList<JGame> games;
-
     public MainModel() {
         games = new ArrayList<>();
-    }
-
-    public void addGame(JGame game) {
-        games.add(game);
-    }
-
-    public void removeGame(JGame game) {
-        games.remove(game);
+        // Add game instances to the list
+        // You can add more games here
+        games.add(new Lemmings("", 800, 600));
+        games.add(new Pong("", 800, 600));
     }
 
     public ArrayList<JGame> getGames() {
