9ea6ce390c78511b9ea2ab049cdaf6e9b3fecd51
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-05-19 14:41:37 -0300

    Modify game screen navbar

--- app/src/main/java/Proyecto/view/Menu/Game_screen/Games_screen.java
+++ app/src/main/java/Proyecto/view/Menu/Game_screen/Games_screen.java
@@ -4,8 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.util.ArrayList;
 
+import javax.swing.Box;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.MatteBorder;
@@ -35,30 +39,45 @@ public class Games_screen extends JPanel {
         game_Button = new game_Button("app\\src\\main\\resources\\images\\games_white.png");
         setting_Button = new settings_Button("app\\src\\main\\resources\\images\\settings.png");
 
-        nav_Panel.setLayout(new FlowLayout());
-        nav_Panel.add(home_Panel);
-        nav_Panel.add(game_Panel);
-        nav_Panel.add(setting_Panel);
-        nav_Panel.setOpaque(false);
         
-        top_Panel.setBackground(new Color(25, 25, 25));
-        top_Panel.setLayout(new BorderLayout());
-        top_Panel.add(nav_Panel, BorderLayout.WEST);
+        home_Button.setPreferredSize(new java.awt.Dimension(25, 25));
+        game_Button.setPreferredSize(new java.awt.Dimension(25, 25));
+        setting_Button.setPreferredSize(new java.awt.Dimension(25, 25));
 
-        home_Button.setPreferredSize(new Dimension(20, 20));
-        home_Panel.setBorder(new MatteBorder(0, 0, 0, 2, new Color(35, 35, 35)));
         home_Panel.add(home_Button);
         home_Panel.setOpaque(false);
-
-        game_Button.setPreferredSize(new Dimension(22, 20));
-        game_Panel.setBorder(new MatteBorder(0, 0, 0, 2, new Color(35, 35, 35)));
         game_Panel.add(game_Button);
         game_Panel.setOpaque(false);
-
-        setting_Button.setPreferredSize(new Dimension(20, 20));
         setting_Panel.add(setting_Button);
         setting_Panel.setOpaque(false);
+
+        GridBagConstraints gbc = new java.awt.GridBagConstraints();
         
+        gbc.gridx = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.NORTH; 
+        
+        gbc.gridy = 0;
+        gbc.insets = new Insets(60, 10, 40, 10); 
+        nav_Panel.add(home_Panel, gbc);
+        
+        gbc.gridy = 1;
+        gbc.insets = new Insets(0, 10, 40, 10);
+        nav_Panel.add(game_Panel, gbc);
+
+        gbc.gridy = 2;
+        gbc.insets = new Insets(0, 10, 0, 10);
+        nav_Panel.add(setting_Panel, gbc);
+        
+        gbc.gridy = 3;
+        gbc.weighty = 1.0;
+        gbc.weighty = 1.0;
+        gbc.fill = GridBagConstraints.VERTICAL;
+
+        nav_Panel.add(Box.createVerticalGlue(), gbc);
+        nav_Panel.setBackground(new Color(34, 35, 37));
+
+
         games_Panel = new JPanel();
         hide = new JPanel();
 
@@ -77,7 +96,7 @@ public class Games_screen extends JPanel {
         }
         this.setLayout(new BorderLayout());
         this.setBackground(new Color(14, 15, 17));
-        this.add(top_Panel, BorderLayout.NORTH);
+        this.add(nav_Panel, BorderLayout.WEST);
         this.add(hide, BorderLayout.CENTER);
     
     }
@@ -94,3 +113,28 @@ public class Games_screen extends JPanel {
             return setting_Button;
         }
 }
+
+
+        // nav_Panel.setLayout(new FlowLayout());
+        // nav_Panel.add(home_Panel);
+        // nav_Panel.add(game_Panel);
+        // nav_Panel.add(setting_Panel);
+        // nav_Panel.setOpaque(false);
+        
+        // top_Panel.setBackground(new Color(25, 25, 25));
+        // top_Panel.setLayout(new BorderLayout());
+        // top_Panel.add(nav_Panel, BorderLayout.WEST);
+
+        // home_Button.setPreferredSize(new Dimension(20, 20));
+        // home_Panel.setBorder(new MatteBorder(0, 0, 0, 2, new Color(35, 35, 35)));
+        // home_Panel.add(home_Button);
+        // home_Panel.setOpaque(false);
+
+        // game_Button.setPreferredSize(new Dimension(22, 20));
+        // game_Panel.setBorder(new MatteBorder(0, 0, 0, 2, new Color(35, 35, 35)));
+        // game_Panel.add(game_Button);
+        // game_Panel.setOpaque(false);
+
+        // setting_Button.setPreferredSize(new Dimension(20, 20));
+        // setting_Panel.add(setting_Button);
+        // setting_Panel.setOpaque(false);
