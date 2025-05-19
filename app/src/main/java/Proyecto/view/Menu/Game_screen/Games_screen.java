086c89b7568feb609f8a75e45d68a1ee9c0f69a0
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-05-18 23:03:18 -0300

    Add image and order games screen

--- app/src/main/java/Proyecto/view/Menu/Game_screen/Games_screen.java
+++ app/src/main/java/Proyecto/view/Menu/Game_screen/Games_screen.java
@@ -15,23 +15,19 @@ import javax.swing.border.MatteBorder;
 
 
 public class Games_screen extends JPanel {
-    final private JPanel games_Panel, hide,top_Panel, title_Panel, back_Panel,home_Panel,game_Panel;
+    final private JPanel games_Panel, hide,top_Panel, title_Panel, home_Button,home_Panel,game_Panel;
     final private JLabel title;
     final private ArrayList<Game> games_Set;
 
     
     public Games_screen() {
-        games_Panel = new JPanel();
-        hide = new JPanel();
+        
         top_Panel = new JPanel();
         home_Panel = new JPanel();
         game_Panel = new JPanel();
         title_Panel = new JPanel();
         title = new JLabel("<html><h2 style='color: white;'>Your Games</h2></html>");
-        games_Set = new ArrayList<>();
-        back_Panel = new back_Panel();
-        this.setLayout(new BorderLayout());
-        this.setBackground(new Color(8, 38, 82)); 
+        
         top_Panel.setOpaque(false);
         top_Panel.setBackground(new Color(16, 44, 85));
         top_Panel.setLayout(new BorderLayout());
@@ -39,21 +35,34 @@ public class Games_screen extends JPanel {
         top_Panel.add(game_Panel, BorderLayout.WEST);
         this.add(top_Panel, BorderLayout.NORTH);
         home_Panel.setLayout(new FlowLayout());
-        back_Panel.setPreferredSize(new Dimension(25, 25));
+        
+        home_Button = new home_Button();
+        
+        home_Button.setPreferredSize(new Dimension(25, 25));
         home_Panel.setBorder(new MatteBorder(0, 0, 0, 2, Color.WHITE));
-        home_Panel.add(back_Panel);
+        home_Panel.add(home_Button);
         home_Panel.setBackground(new Color(16, 44, 85));
         home_Panel.setOpaque(false);
         title_Panel.setOpaque(false);
         title_Panel.add(title);
+
+        this.setLayout(new BorderLayout());
+        this.setBackground(new Color(8, 38, 82));
+        
+        games_Panel = new JPanel();
+        hide = new JPanel();
+
         hide.setOpaque(false);
-        games_Panel.setBackground(new Color(16, 44, 85));
         hide.add(games_Panel);
         this.add(hide, BorderLayout.CENTER);
+        
+        games_Set = new ArrayList<>();
+        games_Panel.setBackground(new Color(16, 44, 85));
         games_Panel.setLayout(new FlowLayout());
         games_Panel.setPreferredSize(getMaximumSize());
         games_Set.add(new Game("app\\\\src\\\\main\\\\resources\\images\\\\Lemmings.jpg", "Lemmings"));
         games_Set.add(new Game("app\\\\src\\\\main\\\\resources\\images\\\\Pong.jpg", "Pong"));
+        
         for (Game g : games_Set) {
             games_Panel.add(g);
         }
@@ -66,8 +75,8 @@ public class Games_screen extends JPanel {
         public void closeGame(){
         closeGameFrame close = new closeGameFrame();
         }
-        public JPanel getBack_Panel(){
-            return back_Panel;
+        public JPanel getBack_Button(){
+            return home_Button;
         }
     }
 
@@ -90,10 +99,36 @@ final private JLabel title;
     }
 }
 
-class back_Panel extends JPanel{
-    private Image background;
-    public back_Panel(){
-        background = new ImageIcon("app\\src\\main\\resources\\images\\arrowback.png").getImage();
+class home_Button extends JPanel{
+    final private Image background;
+    public home_Button(){
+        background = new ImageIcon("app\\src\\main\\resources\\images\\home.png").getImage();
+        this.setOpaque(false); 
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+    }
+}
+
+class game_Button extends JPanel{
+    final private Image background;
+    public game_Button(){
+        background = new ImageIcon("app\\src\\main\\resources\\images\\games.png").getImage();
+        this.setOpaque(false); 
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+    }
+}
+
+class setting_Button extends JPanel{
+    final private Image background;
+    public setting_Button(){
+        background = new ImageIcon("app\\src\\main\\resources\\images\\games.png").getImage();
         this.setOpaque(false); 
     }
     @Override
