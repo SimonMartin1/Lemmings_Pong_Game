62e76db3db32315f9387d406bdf12715793d4cca
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-06-10 16:22:37 -0300

    Add Lemmings settings and delete trash lines

--- app/src/main/java/Proyecto/games/Lemmings_game/Lemmings.java
+++ app/src/main/java/Proyecto/games/Lemmings_game/Lemmings.java
@@ -12,19 +12,26 @@ import java.util.Map;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
+import com.entropyinteractive.JGame;
+
 import Proyecto.games.Lemmings_game.Controller.ButtonController;
 import Proyecto.games.Lemmings_game.Controller.LevelController;
-import Proyecto.games.Lemmings_game.Model.*;
+import Proyecto.games.Lemmings_game.Model.LevelModel;
+import Proyecto.games.Lemmings_game.Model.MapModel;
+import Proyecto.games.Lemmings_game.Model.MinimapModel;
+import Proyecto.games.Lemmings_game.Model.Stock;
 import Proyecto.games.Lemmings_game.Utils.Ability;
-import Proyecto.games.Lemmings_game.Utils.ScoreDatabase;
-import Proyecto.games.Lemmings_game.View.*;
-import com.entropyinteractive.JGame;
+import Proyecto.games.Lemmings_game.View.ExitView;
+import Proyecto.games.Lemmings_game.View.GameMenuView;
+import Proyecto.games.Lemmings_game.View.LevelView;
+import Proyecto.games.Lemmings_game.View.MapView;
+import Proyecto.games.Lemmings_game.View.SpawnerView;
+import Proyecto.games.Lemmings_game.View.GameSettingsView;
 
 
 public class Lemmings extends JGame {
 
-    GameMenuView gameMenuView;
-    private Graphics2D g;
+    GameMenuView gameMenu;
 
     ButtonController buttonController;
 
@@ -35,10 +42,11 @@ public class Lemmings extends JGame {
     private final List<LevelModel> levelModels = new ArrayList<>();
     private final List<LevelView> levelViews = new ArrayList<>();
     private final List<LevelController> levelControllers = new ArrayList<>();
-
+    private GameSettingsView settingsView;
     private final static boolean fullScreen = false;
-    private final int screenWidth = 800;
-    private final int screenHeight = 600;
+    private boolean isInMenu = true, isInSettings=false, gamePause = false, gameOver = false,twoplayers,musicOFF;
+    private final int screenWidth = getWidth();
+    private final int screenHeight = getHeight();
     private final boolean scoreAlreadySaved = false;
 
     public Lemmings(String title, int width, int height) {
@@ -57,6 +65,12 @@ public class Lemmings extends JGame {
         
     }
 
+    public boolean getIsinsettings() {
+        return this.isInSettings;
+    }
+    public void setIsinsettings() {
+        this.isInSettings = !this.isInSettings;
+    }
 
     @Override
     public void gameStartup() {
@@ -66,6 +80,7 @@ public class Lemmings extends JGame {
 
 
         try{
+            settingsView = new GameSettingsView(this.screenWidth, this.screenHeight,this);
             MapModel firstLevelMapModel = new MapModel(1,1070,350,0);
             MapModel secondLevelMapModel = new MapModel(2,650,350,0);
             MapModel thirdLevelMapModel = new MapModel(3,650,350,0);
@@ -135,9 +150,6 @@ public class Lemmings extends JGame {
         levelControllers.add(new LevelController(levelModels.get(1), levelViews.get(1), getKeyboard(), getMouse(), 430, 0, minimapModel, screenWidth, screenHeight));
         levelControllers.add(new LevelController(levelModels.get(2), levelViews.get(2), getKeyboard(), getMouse(), 430, 0, minimapModel, screenWidth, screenHeight));
 
-
-
-
         //Agregamos el listener del mouse
         getFrame().addMouseListener(this.getMouse());
 
@@ -146,38 +158,33 @@ public class Lemmings extends JGame {
         this.getFrame().setIconImage(icon.getImage());
 
         buttonController = new ButtonController(this.getMouse(), screenWidth, screenHeight);
-        gameMenuView = new GameMenuView(getWidth(), getHeight());
+        gameMenu = new GameMenuView(getWidth(), getHeight(), this);
     }
 
     @Override
     public void gameUpdate(double delta) {
-        if (!gameMenuView.isStarting(getMouse()) && !gameMenuView.isStarting(getKeyboard())) {
-            gameMenuView.update(delta);
-        }else{
-            //aca lvl se updatea
-            buttonController.update();
-            levelControllers.get(currentLevel).update(delta);
+        
+        if(isInSettings){
+            //settingController = new SettingController(settingsView, settingsModel, getMouse(), this);
+        }
+
+        if(isInMenu){
+            gameMenu.update(delta);
+            if(gameMenu.detectSetting(getMouse())){ if(isInSettings){/*saveSettings();+/* */} isInSettings = true;  }
+            if((gameMenu.detectPlay(getMouse()) || gameMenu.detectPlay(getKeyboard()))){ isInMenu = false; 
+                buttonController.update(); levelControllers.get(currentLevel).update(delta);}
         }
 
     }
     
     @Override
     public void gameDraw(Graphics2D g) {
-            this.g=g;
-
-            if(!gameMenuView.isStarting(getMouse()) && !gameMenuView.isStarting(getKeyboard())){
-                gameMenuView.draw(g);
+            if(isInMenu){
+            gameMenu.drawmenu(g);
+            if(isInSettings){
+                settingsView.drawmenu(g);
             }
-            else {
-                g.setColor(Color.BLACK);
-                g.fillRect(0, 0, getWidth(), getHeight());
-
-                //Aca segundo lvl
-                levelControllers.get(currentLevel).draw(g);
-
-
             }
-
     }
 
     @Override
--- app/src/main/java/Proyecto/games/Lemmings_game/View/GameMenuView.java
+++ app/src/main/java/Proyecto/games/Lemmings_game/View/GameMenuView.java
@@ -1,29 +1,34 @@
 package Proyecto.games.Lemmings_game.View;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
 
 import com.entropyinteractive.Keyboard;
 import com.entropyinteractive.Mouse;
 
-import java.awt.Graphics2D;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Image;
-
-import javax.swing.ImageIcon;
+import Proyecto.games.Lemmings_game.Lemmings;
 
 public class GameMenuView {
     private final int width;
     private final int height;
     private boolean animation;
+    private double blinkTime;
     private boolean showPressText = true;
-    private double blinkTime = 0;
+    private Boolean prevPausePressed = null;
+    private final Lemmings game;
 
 
-    public GameMenuView(int width, int height) {
+    public GameMenuView(int width, int height, Lemmings game) {
         this.width = width;
         this.height = height;
+        this.game=game;
     }
 
-    public void draw(Graphics2D g) {
+    public void drawmenu(Graphics2D g) {
         Image background = new ImageIcon("app\\src\\main\\resources\\images\\Lemmings_back.png").getImage();
             g.drawImage(background, 0, 0, width, height,null);
             
@@ -31,39 +36,17 @@ public class GameMenuView {
             g.drawImage(lemmings,width/2-290 , 125, width/2+200, 160,null);
 
             Image lemmings_button = new ImageIcon("app\\src\\main\\resources\\images\\Lemmings_button.png").getImage();
-            g.drawImage(lemmings_button,width/2-55 , 275, 120, 120,null);
+            g.drawImage(lemmings_button,width/2-50 , height/2-40, 120, 120,null);
             
             g.setColor(Color.WHITE);
             g.setFont(new Font("Arial", Font.BOLD, 28));
-            g.drawString("Settings", width-250 , 500);
+            g.drawString("Settings", width-250 , height-60);
 
         if (!animation && showPressText) {
             g.setColor(Color.WHITE);
             g.setFont(new Font("Arial", Font.BOLD, 24));
             g.drawString("Click or Enter", width/2 - 71, 420);
         }
-    }
-
-
-    public boolean isStarting(Mouse mouse){
-        if (mouse.isLeftButtonPressed()) {
-            int mx = mouse.getX();
-            int my = mouse.getY();
-            int bx = width/2 - 100, by = 300, bw = 200, bh = 60;
-            if (mx >= bx && mx <= bx + bw && my >= by && my <= by + bh) {
-                animation = true;
-            }
-        }
-
-        return animation;
-    }
-
-    public boolean isStarting(Keyboard k){
-        if (k.isKeyPressed(10)) {
-            animation = true;
-        }
-
-        return animation;
     }
 
     public void update(double delta){
@@ -73,4 +56,33 @@ public class GameMenuView {
             blinkTime = 0;
         }
     }
+
+    public boolean detectPlay(Mouse m) {
+        int mx = m.getX();
+        int my = m.getY();
+        int bx = width/2, by = height/2, bw = 150, bh = 60;
+        return mx >= bx && mx <= bx + bw && my >= by && my <= by + bh && m.isLeftButtonPressed() && !game.getIsinsettings();
+    }
+
+    public boolean detectPlay(Keyboard k){
+        boolean currentPressed = k.isKeyPressed(KeyEvent.VK_ENTER);
+
+        if (prevPausePressed == null) {
+            prevPausePressed = currentPressed;
+            return false;
+        }
+
+        boolean justPressed = currentPressed && !prevPausePressed;
+        prevPausePressed = currentPressed;
+        return justPressed;
+    }
+
+
+    public boolean detectSetting(Mouse m) {
+        int mx = m.getX();
+        int my = m.getY();
+        int bx = width - 250, by = height-110, bw = 150, bh = 80;
+        return mx >= bx && mx <= bx + bw && my >= by && my <= by + bh && m.isLeftButtonPressed() && !game.getIsinsettings();
+    }
+
 }
