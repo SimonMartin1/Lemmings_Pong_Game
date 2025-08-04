1c51b20c23e7e030249c3d4cb6ef859d85656958
Simón Martin <101838886+SimonMartin1@users.noreply.github.com>
2025-08-04 20:59:26 -0300

    Fix homeless conflict

--- app/src/main/java/Proyecto/games/utils/Screen.java
+++ app/src/main/java/Proyecto/games/utils/Screen.java
@@ -3,12 +3,8 @@ package Proyecto.games.utils;
 import java.awt.*;
 
 public abstract class Screen implements Drawable {
-<<<<<<< HEAD
     protected int width,height;
-=======
-    protected int width;
-    protected int height;
->>>>>>> 8db4588f9d1602f37fde3334e3f3fda5728fec42
+
 
     public Screen(int width, int height) {
         this.width = width;
